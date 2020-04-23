2b0f62b209b1f66d5b24e13b73f6c6b7ed42cc23
Edward A Maxedon Sr <edadma@gmail.com>
2020-04-23 00:57:35 -0400

    test code

--- src/main/scala/xyz/hyperreal/pargen/Main.scala
+++ src/main/scala/xyz/hyperreal/pargen/Main.scala
@@ -4,21 +4,33 @@ import scala.util.parsing.input.CharSequenceReader
 
 object Main extends App {
 
-  val input = """array = -"[" ident { -"," ident } -"]" /flatten."""
-//    """
-//      |value = number | string | object | array | "true" | "false" | "null".
-//      |
-//      |object = "{" members "}".
-//      |
-//      |members = member { -"," member } /flatten.
-//      |
-//      |member = string -":" value <member>.
-//      |
-//      |array = value { -"," value } /flatten.
-//      |""".stripMargin
+  val input =
+    """
+      |expression = term { ("+" | "-") term } /infixl.
+      |
+      |term = factor { ("*" | "/") factor } /infixl.
+      |
+      |factor = number
+      |       | ident
+      |       | -"(" expression -")".
+      |""".stripMargin
+
+  //    """
+  //      |value = number | string | object | array | "true" | "false" | "null".
+  //      |
+  //      |object = -"{" members -"}" <object>.
+  //      |
+  //      |members = member { -"," member } /flatten.
+  //      |
+  //      |member = string -":" value.
+  //      |
+  //      |array = -"[" elements -"]" <array>.
+  //      |
+  //      |elements = value { -"," value } /flatten.
+  //      |""".stripMargin
   val ast = SyntaxNotationParser(input)
 
   println(ast)
-  println(Interpreter(ast, new CharSequenceReader(""" "asdf" """)))
+  println(Interpreter(ast, new CharSequenceReader(""" a + b + c """)))
 
 }
