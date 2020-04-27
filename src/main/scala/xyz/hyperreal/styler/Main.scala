cacf5187159f1eda9796cd46b444de67c96ac3e5
Edward A Maxedon Sr <edadma@gmail.com>
2020-04-27 09:17:16 -0400

    json example

--- src/main/scala/xyz/hyperreal/styler/Main.scala
+++ src/main/scala/xyz/hyperreal/styler/Main.scala
@@ -4,23 +4,58 @@ import scala.util.parsing.input.CharSequenceReader
 
 object Main extends App {
 
-//    """
-//      |value = number | string | object | array | "true" | "false" | "null".
-//      |
-//      |object = "{" ^members "}" <object> | "{" "}" <object>.
-//      |
-//      |members = member { "," member } /flatten.
-//      |
-//      |member = string ":" value <member>.
-//      |
-//      |array = "[" ^elements "]" <array> | "[" "]" <array>.
-//      |
-//      |elements = value { "," value } /flatten.
-//      |""".stripMargin
-//  val ast = SyntaxParser(input)
-//
-//  println(ast)
-//  println(Interpreter(ast, new CharSequenceReader(""" {"a": [1], "b": []} """))) // (3 + 4) * 5
+  val jsonSyntax =
+    """
+      |value = number | string | object | array | `true` | `false` | `null`.
+      |
+      |object = "{" ^members "}" <object> | "{" "}" <object>.
+      |
+      |members = member { "," member } /flatten.
+      |
+      |member = string ":" value <member>.
+      |
+      |array = "[" ^elements "]" <array> | "[" "]" <array>.
+      |
+      |elements = value { "," value } /flatten.
+      |""".stripMargin
+  val input = """ {"a": [1], "b": []} """
+
+  val jsonFormat =
+    """
+      |printElem: {
+      |  <const> -> print(const);
+      |  <'number', n> -> print(n);
+      |  <'string', s> -> {
+      |    print('"');
+      |    print(s);
+      |    print('"');
+      |    }
+      |  ['array'] -> print('[]');
+      |  ['array', elements] -> {
+      |    print('[');
+      |    printSeq(elements, ", ");
+      |    print(']');
+      |    }
+      |  ['object'] -> print('{}');
+      |  ['object', members] -> {
+      |    print('{');
+      |    printSeq(members, ", ");
+      |    print('}');
+      |    }
+      |""".stripMargin
+  val sast = SyntaxParser(jsonSyntax)
+
+  println(sast)
+
+  val ast = StylerParser(sast, new CharSequenceReader(input)) getOrElse { println("didn't parse"); sys.exit }
+
+  println(ast)
+
+  val fast = FormatParser(jsonFormat)
+
+  println(fast)
+
+  Interpreter(fast, ast)
 
 //  val input = "123"
 //
@@ -42,60 +77,60 @@ object Main extends App {
 //      |  ['number' n] -> print(n);
 //      |}
 //      |""".stripMargin
-  val input = "  a+((( 5-c ))) * d / e   "
-
-  val syntax =
-    """
-      |expression = term { (`+` | `-`) term } /infixl.
-      |
-      |term = factor { (`*` | `/`) factor } /infixl.
-      |
-      |factor = unary `^` factor /infix
-      |       | unary.
-      |
-      |unary = `-` primary
-      |      | primary.
-      |
-      |primary = number
-      |        | ident
-      |        | "(" expression ")" <group>.
-      |""".stripMargin
-  val sast = SyntaxParser(syntax)
-
-  //  println(sast)
-
-  val ast = StylerParser(sast, new CharSequenceReader(input)) getOrElse sys.error("didn't parse")
-
-  println(ast)
-
-  val format =
-    """
-      |printElem: {
-      |  <'ident'|'number', lit> -> print(lit);
-      |  [op@('+'|'-'), left, right] -> {
-      |    printElem(left);
-      |    print(' ');
-      |    print(op);
-      |    print(' ');
-      |    printElem(right);
-      |    }
-      |  [op@('*'|'/'), left, right] -> {
-      |    printElem(left);
-      |    print(op);
-      |    printElem(right);
-      |    }
-      |  ['group', inner@['group', _]] -> printElem(inner);
-      |  ['group', expr] -> {
-      |    print('(');
-      |    printElem(expr);
-      |    print(')');
-      |    }
-      |}
-      |""".stripMargin
-
-  val fast = FormatParser(format)
-
-//  println(fast)
-  Interpreter(fast, ast)
+//  val input = "  a+((( 5-c ))) * d / e   "
+//
+//  val syntax =
+//    """
+//      |expression = term { (`+` | `-`) term } /infixl.
+//      |
+//      |term = factor { (`*` | `/`) factor } /infixl.
+//      |
+//      |factor = unary `^` factor /infix
+//      |       | unary.
+//      |
+//      |unary = `-` primary
+//      |      | primary.
+//      |
+//      |primary = number
+//      |        | ident
+//      |        | "(" expression ")" <group>.
+//      |""".stripMargin
+//  val sast = SyntaxParser(syntax)
+//
+//  //  println(sast)
+//
+//  val ast = StylerParser(sast, new CharSequenceReader(input)) getOrElse sys.error("didn't parse")
+//
+//  println(ast)
+//
+//  val format =
+//    """
+//      |printElem: {
+//      |  <'ident'|'number', lit> -> print(lit);
+//      |  [op@('+'|'-'), left, right] -> {
+//      |    printElem(left);
+//      |    print(' ');
+//      |    print(op);
+//      |    print(' ');
+//      |    printElem(right);
+//      |    }
+//      |  [op@('*'|'/'), left, right] -> {
+//      |    printElem(left);
+//      |    print(op);
+//      |    printElem(right);
+//      |    }
+//      |  ['group', inner@['group', _]] -> printElem(inner);
+//      |  ['group', expr] -> {
+//      |    print('(');
+//      |    printElem(expr);
+//      |    print(')');
+//      |    }
+//      |}
+//      |""".stripMargin
+//
+//  val fast = FormatParser(format)
+//
+////  println(fast)
+//  Interpreter(fast, ast)
 
 }
